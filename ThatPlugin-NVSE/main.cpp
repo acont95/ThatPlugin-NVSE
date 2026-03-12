1aea2a19596c5521841b1c688a6691dac0d5b99c
Alex Conticello <alexconticello@gmail.com>
2026-03-11 22:22:31 -0400

    Power attacks and automatic melee weapons working.

--- ThatPlugin-NVSE/main.cpp
+++ ThatPlugin-NVSE/main.cpp
@@ -1,7 +1,9 @@
 #include "PluginAPI.h"
 #include "SafeWrite.h"
-#include "GameForms.h"
 #include "GameProcess.h"
+#include "GameObjects.h"
+#include "GameTiles.h"
+#include "Utilities.h"
 #include <cstdint>
 
 #define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
@@ -15,9 +17,10 @@ constexpr uint32_t g_PluginVersion = 100;
 
 CallDetour ObjectHitDetour{};
 CallDetour CombatHitDetour{};
+CallDetour GetCurrentAmmoDetour{};
 
-UInt32 Actor_UseAmmo_Addr = 0x008A89A0;
-UInt32 Actor_GetLastHitData_Addr = 0x008A51A0;
+constexpr UInt32 Actor_UseAmmo_Addr = 0x008A89A0;
+constexpr UInt32 Projectile_Constructor_Addr = 0x8272CC38;
 
 bool __fastcall Hook_IsMeleeWeapon(TESObjectWEAP* weapon, void* edx)
 {
@@ -27,12 +30,14 @@ bool __fastcall Hook_IsMeleeWeapon(TESObjectWEAP* weapon, void* edx)
 	return weapon->eWeaponType <= 2;
 }
 
-int __fastcall Hook_ObjectHit(Actor* actor, void* edx, bool abPowerAttack)
+Tile* __fastcall Hook_ObjectHit(Actor* actor, void* edx, bool abPowerAttack)
 {	
-	int result = ThisStdCall<int>(ObjectHitDetour.GetOverwrittenAddr(), actor, abPowerAttack);
+	Tile* result = ThisStdCall<Tile*>(ObjectHitDetour.GetOverwrittenAddr(), actor, abPowerAttack);
+	TESObjectWEAP* weapon = actor->GetEquippedWeapon();
 
-	//ActorHitData* hitData = ThisStdCall<ActorHitData*>(Actor_GetLastHitData_Addr, actor);
-	if (result) {
+	bool isAutomatic = weapon->weaponFlags1.Extract(1);
+
+	if (weapon && (result || isAutomatic)) {
 		ThisStdCall<void>(Actor_UseAmmo_Addr, actor, 1);
 	}
 
@@ -44,19 +49,33 @@ void __fastcall Hook_CombatHit(
 	void* edx,
 	Actor* apTarget,
 	bool abPowerAttack,
-	void* apProjectile,
+	Projectile* apProjectile,
 	char cMeleeEffect)
 {
 	Actor* actorVar = actor;
+
+	//TESObjectWEAP* weapon = actor->GetEquippedWeapon();
+	//Projectile* projectile = New<Projectile, Projectile_Constructor_Addr>(
+	//	
+	//);
+
 	ThisStdCall<void>(
 		CombatHitDetour.GetOverwrittenAddr(), 
 		actorVar,
 		apTarget, 
 		abPowerAttack, 
-		apProjectile, 
+		apProjectile,
 		cMeleeEffect);
 
 	ThisStdCall<void>(Actor_UseAmmo_Addr, actorVar, 1);
+}
+
+TESAmmo* __fastcall Hook_GetCurrentAmmo(TESObjectWEAP* weapon, void* edx, Actor* apWeaponHolder)
+{
+	if (weapon->eWeaponType <= 2 && weapon->ammo.ammo)
+		return nullptr;
+
+	return ThisStdCall<TESAmmo*>(GetCurrentAmmoDetour.GetOverwrittenAddr(), weapon, apWeaponHolder);
 }
 
 // This is a message handler for nvse events
@@ -126,6 +145,15 @@ EXTERN_DLL_EXPORT bool NVSEPlugin_Load(NVSEInterface* nvse) {
 		WriteRelCall(0x007724CB, UInt32(&Hook_IsMeleeWeapon));
 		ObjectHitDetour.WriteRelCall(0x008997FE, UInt32(&Hook_ObjectHit));
 		CombatHitDetour.WriteRelCall(0x0089996D, UInt32(&Hook_CombatHit));
+
+		// Hook TESObjectWEAP::GetCurrentAmmo inside PlayerCharacter::CheckUserInputAttacks
+		GetCurrentAmmoDetour.WriteRelCall(0x0094926A, UInt32(&Hook_GetCurrentAmmo));
+		GetCurrentAmmoDetour.WriteRelCall(0x009492B5, UInt32(&Hook_GetCurrentAmmo));
+		GetCurrentAmmoDetour.WriteRelCall(0x009492D7, UInt32(&Hook_GetCurrentAmmo));
+		GetCurrentAmmoDetour.WriteRelCall(0x00948E0E, UInt32(&Hook_GetCurrentAmmo));
+		GetCurrentAmmoDetour.WriteRelCall(0x00949E39, UInt32(&Hook_GetCurrentAmmo));
+
+		WriteRelCall(0x0089CD77, UInt32(&Hook_IsMeleeWeapon));
 	}
 
 	return true;
