672d2449ccc64ebe4273051d2bf56bbde4e2f892
Alex Conticello <alexconticello@gmail.com>
2026-08-20 15:53:15 -0400

    Fix ini parsing exception on unicode characters.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <cstdio>
 #include <filesystem>
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <cctype>
+
 #include "nvse/PluginAPI.h"
 #include "BallisticMelee.hpp"
 #include "BetterCounter.hpp"
@@ -15,12 +19,15 @@
 
 #define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
 
+IDebugLog		gLog("logs\\ThatPlugin-NVSE.log");
 PluginHandle	g_pluginHandle = kPluginHandle_Invalid;
 
 NVSEMessagingInterface* g_messagingInterface{};
 NVSEInterface* g_nvseInterface{};
 
+constexpr std::uint32_t PLUGIN_VERSION = 44;
 constexpr char g_PluginVersion[] = "0.4.4";
+
 const std::filesystem::path g_dataPath = "Data";
 const std::filesystem::path g_pluginsPath = g_dataPath / "NVSE/Plugins";
 const std::filesystem::path g_IniFile = "ThatPlugin.ini";
@@ -28,29 +35,41 @@ const std::filesystem::path g_IniFile = "ThatPlugin.ini";
 CSimpleIniA g_Ini;
 bool configMissing;
 
-static std::string normalizePath(const std::filesystem::path path) {
-	std::string pathStr = path.lexically_normal().string();
+static std::wstring normalizePath(const std::filesystem::path& path) {
+	std::wstring pathStr = path.lexically_normal().wstring();
 	std::transform(pathStr.begin(), pathStr.end(), pathStr.begin(),
-		[](unsigned char c) { return std::tolower(c); });
-
+		[](wchar_t c) { return std::tolower(c); });
 	return pathStr;
 }
 
 static void loadAdditionalIniFiles() {
-	std::string pluginsPathStr = normalizePath(g_pluginsPath);
-	std::string iniFileStr = normalizePath(g_IniFile);
+	try {
+		std::wstring pluginsPathStr = normalizePath(g_pluginsPath);
+		std::wstring iniFileStr = normalizePath(g_IniFile);
 
-	// Search the Data directory for additional ini config to merge with main file
-	for (std::filesystem::recursive_directory_iterator it(g_dataPath), end; it != end; ++it) {
-		std::string currPathStr = normalizePath(it->path());
-		std::string currFilenameStr = normalizePath(it->path().filename());
+		for (std::filesystem::recursive_directory_iterator it(g_dataPath), end; it != end; ++it) {
+			std::wstring currPathStr = normalizePath(it->path());
+			std::wstring currFilenameStr = normalizePath(it->path().filename());
 
-		if (it->is_directory() && currPathStr == pluginsPathStr) {
-			it.disable_recursion_pending(); // Don't read the ini again from NVSE/Plugins
+			if (it->is_directory() && currPathStr == pluginsPathStr) {
+				it.disable_recursion_pending();
+			}
+			else if (it->is_regular_file() && currFilenameStr == iniFileStr) {
+				try {
+					Globals::g_Ini.LoadFile(it->path().c_str());
+
+					std::u8string path = it->path().u8string();
+					std::string pathStr(path.begin(), path.end());
+					_MESSAGE("Loaded additional INI file: %s", pathStr.c_str());
+				}
+				catch (const std::exception& e) {
+					_ERROR("Failed to load INI: %s", e.what());
+				}
+			}
 		}
-		else if (it->is_regular_file() && currFilenameStr == iniFileStr) {
-			Globals::g_Ini.LoadFile((it->path()).c_str());
-		}
+	}
+	catch (const std::exception& e) {
+		_ERROR("Failed to search for additional INI files: %s", e.what());
 	}
 }
 
@@ -97,7 +116,7 @@ EXTERN_DLL_EXPORT bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* i
 
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "ThatPluginNVSE";
-	info->version = 044;
+	info->version = PLUGIN_VERSION;
 
 	// version checks
 	if (nvse->nvseVersion < PACKED_NVSE_VERSION)
@@ -110,7 +129,7 @@ EXTERN_DLL_EXPORT bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* i
 }
 
 EXTERN_DLL_EXPORT bool NVSEPlugin_Load(NVSEInterface* nvse) {
-
+	
 	g_pluginHandle = nvse->GetPluginHandle();
 
 	// save the NVSE interface in case we need it later
